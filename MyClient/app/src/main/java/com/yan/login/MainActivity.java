cbc5fdda26f354c62f6e3fc4e2bb34a44d532cc0
张焱 <40714371+YanInfo@users.noreply.github.com>
2019-04-02 14:10:14 +0800

    Update MainActivity.java
    change

--- MyClient/app/src/main/java/com/yan/login/MainActivity.java
+++ MyClient/app/src/main/java/com/yan/login/MainActivity.java
@@ -7,16 +7,11 @@ import android.os.Bundle;
  * @author zhangyan
  * @date 2019/1/10
  */
-
 public class MainActivity extends Activity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
 }
